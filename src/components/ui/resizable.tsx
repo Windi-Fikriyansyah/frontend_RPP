bd250bf289f966c6a901addf35f8931ca3881b36
windi fikriyansyah <diwin321@gmail.com>
2026-01-14 15:28:30 +0700

    baru 1

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { GripVertical } from "lucide-react";
-import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
+import { Group, Panel, Separator } from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
 
 function ResizablePanelGroup({
   className,
   ...props
-}: React.ComponentProps<typeof PanelGroup>) {
+}: React.ComponentProps<typeof Group>) {
   return (
-    <PanelGroup
+    <Group
       className={cn(
         "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
         className
@@ -26,11 +26,11 @@ function ResizableHandle({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof PanelResizeHandle> & {
+}: React.ComponentProps<typeof Separator> & {
   withHandle?: boolean;
 }) {
   return (
-    <PanelResizeHandle
+    <Separator
       className={cn(
         "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 [&[data-panel-group-direction=vertical]>div]:rotate-90",
         className
@@ -42,7 +42,7 @@ function ResizableHandle({
           <GripVertical className="h-2.5 w-2.5" />
         </div>
       )}
-    </PanelResizeHandle>
+    </Separator>
   );
 }
 
